ccc17cee8befcaf500cc0b6397d49226a47c72f6
vxyagr <hello.angkin@gmail.com>
2023-08-11 14:39:02 +0700

    rev

--- components/buyLoka/ReviewCard.jsx
+++ components/buyLoka/ReviewCard.jsx
@@ -118,7 +118,7 @@ const ReviewCard = () => {
               {new Intl.NumberFormat("en-US", {
                 style: "currency",
                 currency: "USD",
-              }).format(investmentValue + LETperDay * 28)}
+              }).format(investmentValue + electricityCostPerDay * 28)}
             </div>
             <div className="w-full p-2 flex text-[#09a668] justify-start items-start text-left hero-lexend text-xl">
               {totalTHRented} TH/s
