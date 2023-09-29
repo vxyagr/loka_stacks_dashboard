b62b05072df9dce241e857d1d81641fb2c80ae73
vxyagr <hello.angkin@gmail.com>
2023-09-29 13:10:33 +0700

    energy in $ tba

--- components/myLoka/EnergyCard.jsx
+++ components/myLoka/EnergyCard.jsx
@@ -34,7 +34,7 @@ const EnergyCard = ({ nft }) => {
       <div className="w-full p-0 pt-4 grid h-3/6 ">
         <RechargeButton buttonText={"RECHARGE"} />
       </div>
-      <div className="w-full p-0 pb-2 grid pt-4    h-1/6">$230 spent</div>
+      <div className="w-full p-0 pb-2 grid pt-4    h-1/6">$TBA spent</div>
     </div>
   );
 };
