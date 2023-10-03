8fee28014e369996cda1e23b60317b78ac06e138
vxyagr <hello.angkin@gmail.com>
2023-10-03 15:24:04 +0700

    mobile mint

--- pages/buy/index.tsx
+++ pages/buy/index.tsx
@@ -43,6 +43,80 @@ const BuyLoka: NextPage = () => {
   );
   const [bitcoinPrice, setBitcoinPrice] = useState(null);
 
+  const hardwareEfficiency = useSelector(
+    (state: any) => state.rootReducer.hardwareEfficiency
+  );
+
+  const currentDurationValue = useSelector(
+    (state: any) => state.rootReducer.duration
+  );
+  const currentDuration = useSelector(
+    (state: any) => state.rootReducer.durationTitle
+  );
+  const powerPerDay: number = useSelector(
+    (state: any) => state.rootReducer.electricityPerDay
+  );
+  const electricityCostPerKwh = useSelector(
+    (state: any) => state.rootReducer.electricityCostPerKwh
+  );
+
+  const totalTHRented = useSelector(
+    (state: any) => state.rootReducer.totalTHRented
+  );
+  const currentDurationDiscount = useSelector(
+    (state: any) => state.rootReducer.durationDiscount
+  );
+
+  //constants
+  const currentSatsMined = useSelector(
+    (state: any) => state.rootReducer.satsMined
+  );
+  const currentBTCMined = (currentSatsMined * 0.00000001).toFixed(6);
+  const [investmentValue, setInvestmentValue] = useState(0);
+  useEffect(() => {
+    setInvestmentValue(currentInvestmentValue);
+  }, [currentInvestmentValue]);
+
+  const [durationValue, setDurationValue] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const [showOverlay, setShowOverlay] = useState(false);
+  const [latestNFTid, setLatestNFTid] = useState(0);
+  const powerperMile = powerPerDay / 1000;
+  const letPD = powerperMile * electricityCostPerKwh * 1000;
+  const [loading, setLoading] = useState(false);
+  const THRentPrice =
+    currentInvestmentValue / (totalTHRented * currentDurationValue * 28);
+  //const LETperDay =
+  //  ((bla) * electricityCostPerKwh).toFixed(2) * 1000;
+  const electricityCostPerDay = letPD / 1000;
+  useEffect(() => {
+    setDurationValue(currentDurationValue);
+    setDuration(currentDuration);
+  }, [currentDurationValue, currentDuration]);
+
+  const buyLoka = async () => {
+    // overlay();
+    //setShowOverlay(true);
+    setLoading(true);
+    setShowOverlay(true);
+    //amount_: Nat, duration_: Nat, hashrate_ : Nat, elec_ : Nat, genesis_ : Nat, start_ : Nat, end_ : N
+    console.log("minting " + parseInt(totalTHRented));
+    const mint = await loka.mintContract(
+      investmentValue,
+      currentDurationValue * 28,
+      currentDuration.toString(),
+      parseInt(totalTHRented),
+      parseInt(Math.ceil(letPD).toFixed(0)),
+      0,
+      1,
+      1
+    );
+
+    setLatestNFTid(parseInt(mint));
+    setLoading(false);
+    console.log("NFT id " + mint + " " + latestNFTid);
+  };
+
   useEffect(() => {
     // Fetch Bitcoin price from CoinGecko API
     axios
@@ -89,7 +163,6 @@ const BuyLoka: NextPage = () => {
         {/* Navigation content */}
         <NavigationBar />
       </nav>
-
       {/* Buy Loka */}
       <div className="flex-grow flex flex-col md:flex-row h-full w-full lg:px-20   mx-auto justify-center items-start text-center">
         <div className="flex-grow flex flex-col md:flex-row lg:max-w-[90%]  mx-auto  ">
@@ -134,9 +207,50 @@ const BuyLoka: NextPage = () => {
           </div>
         </div>
       </div>
+      {showOverlay && (
+        <div className="fixed inset-0 flex items-center justify-center z-50 backdrop-blur-md backdrop-opacity-50 bg-gray-800">
+          <div className="bg-white p-6 rounded-md shadow-lg">
+            {" "}
+            <div>
+              <h3 className="text-xl font-semibold items-center justify-center text-center">
+                {loading ? (
+                  "Mining contract minting in progress..."
+                ) : (
+                  <p>
+                    <div className="p-2">
+                      Successfully minted Loka NFT ID#{latestNFTid}
+                    </div>
+                    <div className="p-2">
+                      Contract amount{" "}
+                      {new Intl.NumberFormat("en-US", {
+                        style: "currency",
+                        currency: "USD",
+                      }).format(investmentValue)}{" "}
+                      for {currentDuration}
+                    </div>
+                    <div className="p-2">
+                      hashrate of{" "}
+                      {new Intl.NumberFormat("en-US", {}).format(totalTHRented)}{" "}
+                      TH
+                    </div>
+                    <button
+                      onClick={() => setShowOverlay(false)}
+                      className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4 ml-4"
+                    >
+                      Close
+                    </button>
+                  </p>
+                )}
+              </h3>
+            </div>
+          </div>
+        </div>
+      )}{" "}
       {connected && genesisNFT ? (
         <div className="w-full sticky-bottom  lg:hidden md:hidden flex bg-custom-blue  min-h-[120px]">
-          <BuyButton />
+          <button className="w-full" onClick={buyLoka}>
+            <BuyButton />
+          </button>
         </div>
       ) : (
         <></>
