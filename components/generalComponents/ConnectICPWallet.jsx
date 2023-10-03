fe59a9b3a35d69267a8a024008d21637f74fb020
vxyagr <hello.angkin@gmail.com>
2023-10-03 12:49:51 +0700

    a

--- components/generalComponents/ConnectICPWallet.jsx
+++ components/generalComponents/ConnectICPWallet.jsx
@@ -142,8 +142,6 @@ const ConnectWallet = () => {
           className="bg-gradient-to-r from-blue-500 rounded-full to-purple-600 text-white lg:text-lg md:text-base text-base  lg:py-1 lg:px-4 py-1 px-4 leading-none tracking-tight hover:bg-left hover:shadow-xl hover:shadow-blue-400/20 active:scale-95 sm:text-2xl  transition duration-300 ease-in-out hover:bg-gradient-to-r hover:from-blue-400 hover:to-purple-500"
         >
           Disconnect
-          <br />
-          {ICPaddr}
         </button>
         {/*<p>mainnet: {userSession.loadUserData().profile.stxAddress.mainnet}</p>
         <p>testnet: {userSession.loadUserData().profile.stxAddress.testnet}</p>*/}
--- components/generalComponents/ConnectICPWalletMobile.jsx
+++ components/generalComponents/ConnectICPWalletMobile.jsx
@@ -140,7 +140,7 @@ const ConnectWalletMobile = () => {
           onClick={disconnect}
           className="w-full  bg-gradient-to-r from-blue-500 rounded-full to-purple-600 text-white lg:text-lg md:text-base text-base  lg:py-1 lg:px-4 py-3 px-4 leading-none tracking-tight hover:bg-left hover:shadow-xl hover:shadow-blue-400/20 active:scale-95 sm:text-2xl  transition duration-300 ease-in-out hover:bg-gradient-to-r hover:from-blue-400 hover:to-purple-500"
         >
-          Disconnect {ICPaddr}
+          Disconnect
         </button>
         {/*<p>mainnet: {userSession.loadUserData().profile.stxAddress.mainnet}</p>
         <p>testnet: {userSession.loadUserData().profile.stxAddress.testnet}</p>*/}
